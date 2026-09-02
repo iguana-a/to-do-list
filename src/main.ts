3d62919da39d001853f745fb327873f6e1b0bb67
Ianna Ryzia <iannaryzia0@gmail.com>
2026-09-01 22:13:01 -0300

    primeiro commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import './style.css'
+
+const lista = document.getElementById('lista') as HTMLUListElement;
+const campoTexto = document.getElementById('campo-texto') as HTMLInputElement;
+const botaoAdicionar = document.getElementById('botao-adicionar') as HTMLButtonElement;
+
+botaoAdicionar.addEventListener('click', ()=>{
+    const li = document.createElement('li');
+    const botaoRemover = document.createElement('button');
+    li.textContent = campoTexto.value;
+    botaoRemover.textContent = 'remover';
+    li.appendChild(botaoRemover);
+    lista.appendChild(li);
+    campoTexto.value='';
+    botaoRemover.addEventListener('click', ()=>{
+        li.remove();
+    })
+});
+
